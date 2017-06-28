d469a08d164bb72526391bf62d8015a501c3f647
Elis Engstrom <elis-engstrom@hotmail.com>
2017-06-28 12:48:46 +0200

    test

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -20,7 +20,7 @@ public class Game extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		// Uppdatera saker här
-
+//Xd
 		batch.begin();
 		// Måla saker här
 		batch.end();
