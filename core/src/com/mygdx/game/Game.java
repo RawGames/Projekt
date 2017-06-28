26b8356d077566c5f85197fc95cbec9b6446f00b
Sebastian Johansson <sebbe.johansson@live.se>
2017-06-28 15:01:46 +0200

    test bild

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.graphics.GameCamera;
 
@@ -12,6 +13,8 @@ public class Game extends ApplicationAdapter {
 	// Graphics
 	SpriteBatch batch;
 	GameCamera cam;
+
+	Texture img;
 
 	public final static int WIDTHT = 180;
 	public final static int HEIGHT = 320;
@@ -25,11 +28,12 @@ public class Game extends ApplicationAdapter {
 		cam.setPosition(0,0);
 		cam.update();
 
+		img = new Texture("test.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.3f, 0.55f, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(cam.projection);
 		// Uppdatera saker här
@@ -40,13 +44,13 @@ public class Game extends ApplicationAdapter {
 
 		batch.begin();
 		// Måla saker här
-
+		batch.draw(img, 0,0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-
+		img.dispose();
 	}
 }
