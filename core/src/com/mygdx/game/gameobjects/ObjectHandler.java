108340359fe12824ac86eff003b3cd68006acc1d
Sebastian Johansson <sebbe.johansson@live.se>
2017-06-28 18:08:57 +0200

    top kek

---
+++ core/src/com/mygdx/game/gameobjects/ObjectHandler.java
@@ -0,0 +1,26 @@
+package com.mygdx.game.gameobjects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by sebastianjohansson on 2017-06-28.
+ */
+public class ObjectHandler {
+
+    public ObjectHandler(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void draw(SpriteBatch batch){
+
+    }
+
+    public void dispose(){
+
+    }
+
+}
