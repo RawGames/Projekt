ce6cce9b7dcac51e5cc95fece83881823355bb42
Sebastian Johansson <sebbe.johansson@live.se>
2017-06-28 18:21:16 +0200

    immersive af

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -9,8 +9,11 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		config.hideStatusBar = true;
+		config.useImmersiveMode=true;
+
 		initialize(new Game(), config);
-
-
 	}
+
 }
