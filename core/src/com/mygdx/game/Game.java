90871e36bbf80dc4971c50281787c1a60f82f779
Elis Engstrom <elis-engstrom@hotmail.com>
2017-06-28 12:46:43 +0200

    Start koden eller något xd

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -3,11 +3,13 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -21,7 +23,6 @@ public class Game extends ApplicationAdapter {
 
 		batch.begin();
 		// Måla saker här
-
 		batch.end();
 	}
 	
